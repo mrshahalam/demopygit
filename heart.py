8e31718d52dab0a2a781c3d12b85bfb0db60b2e2
Shah Alam <52881138+mrshahalam@users.noreply.github.com>
2019-08-02 18:49:20 +0530

    heart design with turtle

---
+++ heart.py
@@ -0,0 +1,15 @@
+# ❤❤❤❤❤❤❤  in python
+import  turtle
+t = turtle.Turtle()
+t.color('red')
+t.begin_fill()
+t.fillcolor('red')
+t.left(140)
+t.forward(180)
+t.circle(-90,210)
+t.left(140)
+t.circle(-90,210)
+t.forward(180)
+t.end_fill()
+t.hideturtle()
+
